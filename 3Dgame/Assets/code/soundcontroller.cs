b94998589973cf6febe7cd837548e2ff88a2f9cf
31good <45097407+31good@users.noreply.github.com>
2021-11-11 01:53:20 -0500

    sound effect added

---
+++ 3Dgame/Assets/code/soundcontroller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundcontroller : MonoBehaviour
+{
+    public AudioSource fight;
+    public AudioSource inprogress;
+
+    // Update is called once per frame
+    void Start(){
+        inprogress.Play();
+        fight.Stop();
+    }
+    void Update()
+    {
+        //print(GameObject.FindWithTag("bullet"));
+        if(GameObject.FindWithTag("bullet")&&!fight.isPlaying){
+            //print("true");
+            fight.volume=1;
+            inprogress.Pause();
+            fight.Play();
+        }
+        if(!GameObject.FindWithTag("bullet")&&!inprogress.isPlaying){
+            StartCoroutine(Wait());
+        }
+ 
+    }
+    private IEnumerator Wait()
+    {
+    // Play the animation for getting suck in
+        fight.volume-=(Time.deltaTime/3);
+        yield return new WaitForSeconds(5);
+        inprogress.UnPause();
+        fight.Stop();
+    // Move this object somewhere off the screen
+
+    }
+}
